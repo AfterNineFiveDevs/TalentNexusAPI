961e495e7b9f7a52d63b0eb2ae3647d51a1fd769
Han Lin Aung <hanlinthedeveloper@gmail.com>
2026-09-24 00:27:11 +0630

    fix errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core/constants';
+import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
