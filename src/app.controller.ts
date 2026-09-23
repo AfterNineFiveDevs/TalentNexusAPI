7604f0c25eed89b957f5974507119e79c0b8de65
Han Lin Aung <hanlinthedeveloper@gmail.com>
2026-09-23 23:44:01 +0630

    with errors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request } from '@nestjs/common';
+import { CurrentUser } from './@decorators/current-user.decorator';
+import { Public } from './@decorators/public.decorator';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly authService: AuthService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Public()
+  @Post('auth/login')
+  async login(@Request() req: any) {
+    return this.authService.login(req.user);
+  }
+
+  @Post('auth/logout')
+  async logout(@Request() req: any) {
+    await new Promise<void>((resolve, reject) =>
+      req.logout((err: any) => (err ? reject(err) : resolve())),
+    );
+  }
+
+  @Get('profile')
+  getProfile(@CurrentUser() user: any) {
+    return user;
+  }
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core/constants';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule,ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+  }), ConfigModule.forRoot({
+      isGlobal: true,
+    })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard,
+  }],
 })
 export class AppModule {}
